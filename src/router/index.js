abbd68317ae32aca79249adf2f931aed197689c4
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-04 23:24:21 +0300

    App views and router configuration.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Cart from "@/views/Cart";
+import Checkout from "@/views/Checkout";
+import Menu from "@/views/Menu";
 
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Menu
+    },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart
+    },
+    {
+        path: '/checkout',
+        name: 'Checkout',
+        component: Checkout
+    },
+];
 
 const router = new VueRouter({
     routes
