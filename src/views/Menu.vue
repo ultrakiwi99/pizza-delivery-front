1b6fd7acc5c38a28f91ce0c39a1d5eaf63d2423d
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-05 15:31:02 +0300

    Menu api request setup.

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,17 +1,28 @@
 <template>
     <ContainerRowColumn>
+        <CartIcon/>
         <Categories :categories="categories"/>
-
+        <ProductCard/>
     </ContainerRowColumn>
 </template>
 
 <script>
     import ContainerRowColumn from "@/components/Helpers/ContainerRowColumn";
     import Categories from "@/components/Menu/Categories";
+    import ProductCard from "@/components/Menu/ProductCard";
+    import CartIcon from "@/components/Menu/CartIcon";
 
     export default {
         name: "Menu",
-        components: {ContainerRowColumn, Categories}
+        components: {CartIcon, ProductCard, ContainerRowColumn, Categories},
+        mounted() {
+            this.$api
+                .getCategoriesAndProducts()
+                .then(result => this.categoriesAndProducts = result);
+        },
+        data: () => ({
+            categoriesAndProducts: null
+        })
     }
 </script>
 
